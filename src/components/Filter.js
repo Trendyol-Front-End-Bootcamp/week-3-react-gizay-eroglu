b063103fdf9acd3f0451d651d4fa7028096b18f5
gizayeroglu <61948417+gizayeroglu@users.noreply.github.com>
2021-07-17 00:32:28 +0300

    filter component has been styled

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Filter({ filter }) {
-  const [gender, setGender] = useState('female');
-  const [name, setName] = useState('');
-  const [status, setStatus] = useState('alive');
+  const [gender, setGender] = useState("female");
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("alive");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -12,35 +12,51 @@ function Filter({ filter }) {
 
   const handleNameChange = (event) => {
     setName(event.target.value);
-  }
+  };
 
   const handleGenderChange = (event) => {
-    setGender(event.target.value)
-  }
+    setGender(event.target.value);
+  };
 
   const handleStatusChange = (event) => {
-    setStatus(event.target.value)
-  }
+    setStatus(event.target.value);
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form className='filtersClass' onSubmit={handleSubmit}>
         <label htmlFor='name'>Name:</label>
-        <input type='text' id='name' name='name' value={name} onChange={handleNameChange}/>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          value={name}
+          onChange={handleNameChange}
+        />
         <label htmlFor='gender'>Gender:</label>
-        <select value={gender} name='gender' id='gender' onChange={handleGenderChange}>
+        <select
+          value={gender}
+          name='gender'
+          id='gender'
+          onChange={handleGenderChange}
+        >
           <option value='female'>Female</option>
           <option value='male'>Male</option>
           <option value='genderless'>Genderless</option>
           <option value='unknown'>Unknown</option>
         </select>
         <label htmlFor='status'>Status:</label>
-        <select value={status} name='status' id='status' onChange={handleStatusChange}>
+        <select
+          value={status}
+          name='status'
+          id='status'
+          onChange={handleStatusChange}
+        >
           <option value='alive'>Alive</option>
           <option value='dead'>Dead</option>
           <option value='unknown'>Unknown</option>
         </select>
-        <input type="submit" value="Filter" />
+        <input type='submit' value='Filter' />
       </form>
     </>
   );
