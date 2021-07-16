84f556b4cc251088283a041bad7a9979c34a6b2f
Gizay Eroğlu <gizayeroglu1@hotmail.com>
2021-07-16 21:57:18 +0300

    Filter component implemented

--- src/App.js
+++ src/App.js
@@ -7,30 +7,52 @@ import { useEffect, useState } from 'react';
 function App() {
   const [characters, setCharacters] = useState([]);
 
-  const getCharacters = async () => {
-    const response = await axios.get('https://rickandmortyapi.com/api/character');
+  const getAllCharacters = async () => {
+    const response = await axios.get(
+      'https://rickandmortyapi.com/api/character'
+    );
     setCharacters(response.data.results);
-  }
-  
+  };
+
+  const filterCharacters = async (name, gender, status) => {
+    const nameParam = name ? `name=${name}&` : '';
+    const genderParam = gender ? `gender=${gender}&` : '';
+    const statusParam = status ? `status=${status}` : '';
+
+    try {
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character/?${nameParam}${genderParam}${statusParam}`
+      );
+
+      setCharacters(response.data.results);
+    } catch (error) {
+      setCharacters([]);
+    }
+  };
+
   useEffect(() => {
-    getCharacters();
+    getAllCharacters();
   }, []);
-  
+
   return (
     <>
-      <Filter />
-      {characters.map((character) => {
-        return (
-          <Card
-            key={character.id}
-            image={character.image}
-            name={character.name}
-            status={character.status}
-            gender={character.gender}
-            species={character.species}
-          />
-        );
-      })}
+      <Filter filter={filterCharacters} />
+      {characters.length > 0 ? (
+        characters.map((character) => {
+          return (
+            <Card
+              key={character.id}
+              image={character.image}
+              name={character.name}
+              status={character.status}
+              gender={character.gender}
+              species={character.species}
+            />
+          );
+        })
+      ) : (
+        <h4>Not Found</h4>
+      )}
     </>
   );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,8 +1,49 @@
-function Filter() {
- 
- return (
-   <h1>My filter component</h1>
- )
+import { useState } from 'react';
+
+function Filter({ filter }) {
+  const [gender, setGender] = useState('female');
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('alive');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    filter(name, gender, status);
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value)
+  }
+
+  const handleStatusChange = (event) => {
+    setStatus(event.target.value)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name:</label>
+        <input type='text' id='name' name='name' value={name} onChange={handleNameChange}/>
+        <label htmlFor='gender'>Gender:</label>
+        <select value={gender} name='gender' id='gender' onChange={handleGenderChange}>
+          <option value='female'>Female</option>
+          <option value='male'>Male</option>
+          <option value='genderless'>Genderless</option>
+          <option value='unknown'>Unknown</option>
+        </select>
+        <label htmlFor='status'>Status:</label>
+        <select value={status} name='status' id='status' onChange={handleStatusChange}>
+          <option value='alive'>Alive</option>
+          <option value='dead'>Dead</option>
+          <option value='unknown'>Unknown</option>
+        </select>
+        <input type="submit" value="Filter" />
+      </form>
+    </>
+  );
 }
 
-export default Filter; 
+export default Filter;
